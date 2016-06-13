d55634c377a7f2a76b976d1d4cfb380b84c997f7
rhc912 <rhc912@gmail.com>
2016-06-13 13:53:58 -0400

    Semi-complete

---
+++ app/scripts/githubapikey.js
@@ -0,0 +1,2 @@
+
+module.exports = {"token": '34fd4bec751de95c746d820d703bc490c1315bde'}
---
+++ app/scripts/index.js
@@ -0,0 +1,54 @@
+var $ = require('jquery');
+var handlebars = require('handlebars');
+
+var githubtoken = require('./githubapikey.js');
+
+if (githubtoken !== undefined){
+
+  $.ajaxSetup({
+    headers: {
+      'Authorization': 'token ' + githubtoken.token
+    }
+  });
+}
+
+var baseUrl = "https://api.github.com"
+
+
+  var userUrl = baseUrl + "/users/rhc912";
+  //Populates Sidebar Profile;
+  var sidebarTemplate = $('#sidebarTemplate').html();
+  var template = handlebars.compile(sidebarTemplate)
+
+
+
+
+  $.ajax(userUrl).done(function(data){
+    console.log(data);
+
+    var sortedData = template(data);
+    $('.js-sidebar-data').append(sortedData);
+
+  })
+  /*$.ajax(repoUrl).done(function(data){
+    console.log(data);
+  });
+};
+fetchData();*/
+
+//Repo Template
+var repoUrl = baseUrl + "/users/rhc912/repos";
+var repoTemp = $('#repotemplate').html();
+var repoform = handlebars.compile(repoTemp);
+
+$.ajax(repoUrl).done(function(repos){
+  console.log(repos);
+  repos.forEach(function(repo){
+    console.log(repo);
+
+    var renderTemplate = repoform(repo);
+    $('.js-repo-list').append(renderTemplate);
+  });
+
+
+});
